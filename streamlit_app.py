5759cd73650ae4636a94944c3ac2fddccc452ae1
amanda0103 <132141444+amanda0103@users.noreply.github.com>
2023-04-29 08:30:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('BREAKFAST MENU')
-'Omega 3 and blueberry oatmeal'
+streamlit.text('Omega 3 and blueberry oatmeal')
 'Kale,Spinach and Rocket Smoothie'
 'Hard-boiled free-ranged eggs'
