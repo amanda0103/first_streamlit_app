0aee52862a66935d604439a49a3e1115a2951075
amanda0103 <132141444+amanda0103@users.noreply.github.com>
2023-04-30 07:45:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 
